36c3e59cd45886fcb5cfbbdfe47e7c7943c5df33
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-09-11 09:14:11 +0200

    oops

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import './style.scss'
-import Main from "@/Main";
-import PacMan from "@/players/PacMan";
+import Main from '@/Main'
+import PacMan from '@/players/PacMan'
 import One from '@/Maps/One'
 
 new Main({
-    players: [new PacMan()],
-    map: new One()
+    map: new One(),
+    players: [new PacMan()]
 })
--- src/players/PacMan.ts
+++ src/players/PacMan.ts
@@ -1,5 +1,5 @@
-import Player from '@/Players'
 import * as PIXI from 'pixi.js'
+import Player from '@/Players'
 
 export default class PacMan extends Player {
     PacMan1
@@ -12,9 +12,14 @@ export default class PacMan extends Player {
     }
 
     setup() {
+        const base = new PIXI.Graphics()
+        base.width = 30
+        base.height = 30
+        base.alpha = 0
+
         const PacMan1 = (this.PacMan1 = new PIXI.Graphics())
         PacMan1.beginFill(0xff9900)
-        PacMan1.arc(0, 0, 60, 0, -Math.PI / 5, false)
+        PacMan1.arc(0, 0, 60, Math.PI / 5, -Math.PI / 5, false)
         PacMan1.lineTo(0, 0)
         PacMan1.endFill()
         PacMan1.x = 0
@@ -22,12 +27,13 @@ export default class PacMan extends Player {
 
         const PacMan2 = (this.PacMan2 = new PIXI.Graphics())
         PacMan2.beginFill(0xff9900)
-        PacMan2.arc(0, 0, 30, 0, -Math.PI / 15, false)
+        PacMan2.arc(0, 0, 60, Math.PI / 15, -Math.PI / 15, false)
         PacMan2.lineTo(0, 0)
         PacMan2.endFill()
         PacMan2.x = 0
         PacMan2.y = 0
 
+        this.container.addChild(base)
         this.container.addChild(PacMan1)
         this.container.addChild(PacMan2)
 
@@ -39,7 +45,6 @@ export default class PacMan extends Player {
 
     draw() {
         if (this.state === Player.RUNNING && this.customLoopTimer++ > 4) {
-            console.log(this.PacMan1.visible)
             this.PacMan2.visible = !this.PacMan2.visible
             this.customLoopTimer = 0
         }
